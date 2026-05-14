18e763bce0031b1841b289fec0700dc49937fbad
D292660 <dyepes@hiberus.com>
2026-05-14 18:16:32 +0200

    DateRange ValueObject

---
+++ Src/SharedKernel/Fractalize.Ddd/Fractalize.Ddd.SharedKernel/DateRange.cs
@@ -0,0 +1,24 @@
+﻿namespace Fractalize.Ddd.SharedKernel;
+
+public record DateRange : ValueObjectBase
+{
+    public DateTimeOffset Start { get; set; }
+    public DateTimeOffset End { get; set; }
+
+    public DateRange(DateTimeOffset startDate, DateTimeOffset endDate)
+    {
+        if(endDate <= startDate)
+        {
+            throw new ArgumentOutOfRangeException($"{nameof(endDate)} should be higher than {startDate}");
+        }
+        Start = startDate;
+        End = endDate;
+    }
+
+    private DateRange()
+    {
+    }
+
+    public bool Overlaps(DateRange dateRange) =>
+        Start < End && End > dateRange.Start;
+}
