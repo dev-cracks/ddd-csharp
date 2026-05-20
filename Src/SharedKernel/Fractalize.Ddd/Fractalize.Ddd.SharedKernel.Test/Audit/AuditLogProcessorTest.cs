3dbf42ec7c2ed216d995d123e8eee11858629c41
Franklin Jose Alarza Moreno <franklinalarzamoreno@gmail.com>
2026-05-20 18:51:04 +0200

    feat(auditLog) base for auditLog processor

---
+++ Src/SharedKernel/Fractalize.Ddd/Fractalize.Ddd.SharedKernel.Test/Audit/AuditLogProcessorTest.cs
@@ -0,0 +1,58 @@
+﻿using Fractalize.Ddd.SharedKernel.Audit;
+using Moq;
+using Xunit.Categories;
+
+namespace Fractalize.Ddd.SharedKernel.Test.Audit;
+
+public class AuditLogProcessorTest
+{
+    [Fact]
+    [UnitTest]
+    public async Task ProcessEventAsync_WithRightConfiguration_CallsGetCurrentTimeAndSaveEventAsync()
+    {
+        //Arrange
+        var IClockStampMock = new Mock<IClockStamp>();
+        var IAuditRepositoryMock = new Mock<IAuditRepository>();
+        IReadOnlyCollection<AuditedEvent> eventList = new List<AuditedEvent>();
+        AuditLogProcessor auditLogProcessor = new AuditLogProcessor(eventList, IClockStampMock.Object, IAuditRepositoryMock.Object);
+
+        //Act
+        await auditLogProcessor.ProcessEventAsync(new AuditedEvent()
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTimeOffset.UtcNow,
+            UserId = Guid.NewGuid(),
+        });
+
+        //Assert
+        IClockStampMock.Verify(x => x.GetCurrentTime(), Times.Once);
+        IAuditRepositoryMock.Verify(x => x.SaveEventAsync(It.IsAny<AuditedEvent>()), Times.Once);
+    }
+
+    [Fact]
+    [UnitTest]
+    public async Task ProcessEventAsync_GettingTimeFromClockStamp_IsUseAsAuditEventCreationTime()
+    {
+        //Arrange
+        var eventTarget = new AuditedEvent()
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = default,
+            UserId = Guid.NewGuid(),
+        };
+        DateTimeOffset expectedCreationTime = DateTimeOffset.UtcNow;
+        var IClockStampMock = new Mock<IClockStamp>();
+        IClockStampMock.Setup(x => x.GetCurrentTime()).Returns(expectedCreationTime);
+
+        var IAuditRepositoryMock = new Mock<IAuditRepository>();
+        IReadOnlyCollection<AuditedEvent> eventList = new List<AuditedEvent>();
+        AuditLogProcessor auditLogProcessor = new AuditLogProcessor(eventList, IClockStampMock.Object, IAuditRepositoryMock.Object);
+
+        //Act
+        await auditLogProcessor.ProcessEventAsync(eventTarget);
+
+        //Assert
+        IClockStampMock.Verify(x => x.GetCurrentTime(), Times.Once);
+        IAuditRepositoryMock.Verify(x => x.SaveEventAsync(It.Is<AuditedEvent>(ev => ev.CreatedAt == expectedCreationTime)), Times.Once);
+    }
+}
---
+++ Src/SharedKernel/Fractalize.Ddd/Fractalize.Ddd.SharedKernel/Audit/AuditLogProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace Fractalize.Ddd.SharedKernel.Audit;
+
+public class AuditLogProcessor(
+    IReadOnlyCollection<AuditedEvent> eventList, 
+    IClockStamp clockStamp, 
+    IAuditRepository auditRepository)
+{
+    public async Task ProcessEventAsync(AuditedEvent auditedEvent)
+    {
+        // Process the audited event (e.g., log it, store it in a database, etc.)
+        Console.WriteLine($"Processing event: Id={auditedEvent.Id}, Date={auditedEvent.CreatedAt}, UserId={auditedEvent.UserId}");
+
+        auditedEvent.CreatedAt = clockStamp.GetCurrentTime();
+        await auditRepository.SaveEventAsync(auditedEvent);
+    }
+}
+
+public record AuditedEvent 
+{
+    public required Guid Id { get; set; }
+
+    public required DateTimeOffset CreatedAt { get; set; }
+
+    public required Guid UserId { get; set; }
+}
